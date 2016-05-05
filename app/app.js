bd3df6b0da247406d50eaaa2ffc79630e0fbbd0b
Han <hanchen333@gmail.com>
2016-05-04 22:37:55 -0500

    initial draft and commit of front end

---
+++ app/app.js
@@ -0,0 +1,45 @@
+var myApp = angular.module('myApp', ['ngRoute', 'angular.filter']).constant('_', window._);
+
+myApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/login', {
+            templateUrl: '/partials/login.html',
+            controller: 'loginController',
+            access: {restricted: false}
+        })
+        .when('/logout', {
+            controller: 'logoutController',
+            access: {restricted: true}
+        })
+        .when('/register', {
+            templateUrl: '/partials/register.html',
+            controller: 'registerController',
+            access: {restricted: false}
+        })
+        .when('/user', {
+            templateUrl: '/partials/user.html',
+            controller: 'userController',
+            access: {restricted: true}
+        })
+        .when('/list', {
+            templateUrl: '/partials/list.html',
+            controller: 'listController',
+            access: {restricted: true}
+        })
+        .otherwise({
+            redirectTo: '/login'
+        });
+});
+
+// myApp.run(function ($rootScope, $location, $route, AuthService) {
+//     $rootScope.$on('$routeChangeStart',
+//         function (event, next, current) {
+//             AuthService.getUserStatus()
+//                 .then(function(){
+//                     if (next.access.restricted && !AuthService.isLoggedIn()){
+//                         $location.path('/');
+//                         $route.reload();
+//                     }
+//                 });
+//         });
+// });
