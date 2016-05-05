160d6418a008324ce54f4238fc3658edb43a75f2
Jeffrey Tolar <tolar.jeffrey@gmail.com>
2016-05-04 19:58:10 -0500

    Add not implemented error

--- api.go
+++ api.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"errors"
 	"io"
 	"net/http"
 
 	"golang.org/x/net/context"
 )
+
+var ErrNotImplemented = errors.New("not implemented")
 
 func notImplemented(ctx context.Context, rw http.ResponseWriter, r *http.Request) {
 	io.WriteString(rw, "not implemented")
