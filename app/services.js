bb1c6e65b631a0edd9898b007bc17d5b8a7004d3
Adam Madsen <madsen.adam@gmail.com>
2016-05-06 16:48:12 -0500

    Remove logged-in logging.

--- app/services.js
+++ app/services.js
@@ -3,8 +3,8 @@ angular.module('myApp').factory('AuthService',
         function ($q, $timeout, $http) {
 
             function isLoggedIn() {
-                console.log("isloggedin?");
-                console.log(user);
+                // console.log("isloggedin?");
+                // console.log(user);
                 if(user) {
                     return true;
                 } else {
